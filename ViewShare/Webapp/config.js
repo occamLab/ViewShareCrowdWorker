fe79ef8d786b6d8f7469dc32fc03ec88a5f10bbe
dberny <diego.berny@students.olin.edu>
2018-06-25 09:48:17 -0400

    Added current version of website to repo

---
+++ ViewShare/Webapp/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBakHl5BORexlsuMT2jUxi5PgQOnuKSco8",
+authDomain: "object-locator.firebaseapp.com",
+databaseURL: "https://object-locator.firebaseio.com",
+projectId: "object-locator",
+storageBucket: "object-locator.appspot.com",
+messagingSenderId: "876037163064"
+};
+firebase.initializeApp(config);
